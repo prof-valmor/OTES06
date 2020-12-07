01496209ae975a5a33dba830137f640f0ebef161
adamiv <valmor_adami_jr@whirlpool.com>
2020-12-07 20:44:23 -0300

    adicionando as telas novas.

--- ControleGeladeira/app/src/main/java/br/com/oprofvalmor/controlegeladeira/SelecaoDeTemperatura.java
+++ ControleGeladeira/app/src/main/java/br/com/oprofvalmor/controlegeladeira/SelecaoDeTemperatura.java
@@ -2,7 +2,12 @@ package br.com.oprofvalmor.controlegeladeira;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SelecaoDeTemperatura extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class SelecaoDeTemperatura extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selecao_de_temperatura);
+
+        SeekBar sbTemperature = findViewById(R.id.sbTemperature);
+        Button  btNext = findViewById(R.id.btNext);
+        TextView txtTemperature = findViewById(R.id.txtTemperature);
+        txtTemperature.setText(OpcoesDeTemperatura.MEDIA.getMeuNome());
+
+        btNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MonitoramentoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        sbTemperature.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int p = seekBar.getProgress();
+                txtTemperature.setText(OpcoesDeTemperatura.getNome(p));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
