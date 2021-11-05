06e78a24b5e4f342f110b0bb6c6d2b34c46e0df2
Valmor <valmor_adami_jr@whirlpool.com>
2021-11-04 22:39:26 -0300

    adding DB

--- DataStorage/app/src/main/java/br/udesc/datastorage/MainActivity.java
+++ DataStorage/app/src/main/java/br/udesc/datastorage/MainActivity.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import br.udesc.datastorage.model.GerenciadorDeDados;
 import br.udesc.datastorage.model.Pessoa;
@@ -21,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Criando o DB
+        GerenciadorDeDados.getInstance().initDatabase(getApplicationContext());
+        //
         Button btSalvar = findViewById(R.id.btSalvar);
         Button btLimpar = findViewById(R.id.btLimpar);
         btLimpar.setOnClickListener(view -> {
@@ -32,16 +39,32 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 EditText edtNome = findViewById(R.id.edtNome);
                 String nome = edtNome.getText().toString();
+                //
+                Pessoa novaPessoa = new Pessoa(nome, "ficticio nr 200");
                 if(!nome.isEmpty()) {
-                    Pessoa p = new Pessoa(nome);
-                    try {
-                        GerenciadorDeDados.getInstance().armazenarPessoaEmArquivo(getApplicationContext(), p);
-                    }
-                    catch(FileNotFoundException e) {
-                        Log.d(TAG, "onClick: nao foi possivel armazenar. " + e.getMessage());
-                    }
+                    Runnable r = new Runnable() {
+                        @Override
+                        public void run() {
+                            GerenciadorDeDados.getInstance().getDatabase().getPessoaDAO().add(novaPessoa);
+                        }
+                    };
+                    new Thread(r).start();
                 }
             }
         });
+
+        initTask();
+    }
+    private void initTask() {
+        Timer t = new Timer();
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                List<Pessoa> lista = GerenciadorDeDados.getInstance().getDatabase().getPessoaDAO().getAll();
+                for(Pessoa p : lista) {
+                    Log.d(TAG, "PESSOA: " + p);
+                }
+            }
+        }, 0, 1000);
     }
 }
--- DataStorage/app/src/main/java/br/udesc/datastorage/model/Pessoa.java
+++ DataStorage/app/src/main/java/br/udesc/datastorage/model/Pessoa.java
@@ -1,10 +1,26 @@
 package br.udesc.datastorage.model;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Pessoa {
+    @PrimaryKey @NonNull
     private String nome;
-    public Pessoa(String nome) {
+    private String endereco;
+
+    public Pessoa(@NonNull String nome, String endereco) {
         this.nome = nome;
+        this.endereco = endereco;
     }
+    public String getNome() {
+        return nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+
     public String toString() {
         return nome;
     }
