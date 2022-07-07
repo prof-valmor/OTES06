3eed4a5b790d680eeddaae21d76b13f08b2230e8
prof-valmor <o.prof.valmor@gmail.com>
2022-07-07 20:58:33 -0300

    adicionando codigo para requisitar imagem.

--- 2022-01/PokeAPI/app/src/main/java/com/example/pokeapi/model/PokeApiReader.java
+++ 2022-01/PokeAPI/app/src/main/java/com/example/pokeapi/model/PokeApiReader.java
@@ -1,13 +1,20 @@
 package com.example.pokeapi.model;
 
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
 import androidx.lifecycle.MutableLiveData;
 
 import com.android.volley.Request;
+import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.StringRequest;
 
 class PokeApiReader {
     public static MutableLiveData<String> data = new MutableLiveData<>();
+    public static MutableLiveData<Bitmap> image = new MutableLiveData<>();
 
     public static void requestData(String pokemonName) {
         String url = "https://pokeapi.co/api/v2/pokemon/"+pokemonName;
@@ -20,6 +27,31 @@ class PokeApiReader {
             }
         );
         VolleyQueueManager.addToQueue(request);
-
+    }
+    public static void requestImage(String imageId) {
+        String url = "https:"+imageId;
+//        String url,
+//        Response.Listener<Bitmap> listener,
+//        int maxWidth,
+//        int maxHeight,
+//        ImageView.ScaleType scaleType,
+//        Bitmap.Config decodeConfig,
+//        @Nullable Response.ErrorListener errorListener
+        ImageRequest request = new ImageRequest(url,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        image.postValue(response);
+                    }
+                },
+                300, 300,
+                ImageView.ScaleType.CENTER_CROP,
+                Bitmap.Config.RGB_565,
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        image.postValue(null);
+                    }
+                });
     }
 }
